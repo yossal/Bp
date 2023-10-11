de9b818cb858e13b527a23329c76f6fd4ad59685
yossal <147320092+yossal@users.noreply.github.com>
2023-10-11 13:43:29 +0700

    Add files via upload

---
+++ SegitigaAngka.java
@@ -0,0 +1,11 @@
+package com.mycompany.segitigaangka;
+public class SegitigaAngka {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println("");
+        }
+    }
+}
